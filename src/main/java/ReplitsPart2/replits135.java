31151611fb1f99e38a100dbcff598c6dfbceffbd
ahmedyazeed86 <ahmedyazeed86@gmail.com>
2023-09-08 18:04:41 -0400

    third commit

---
+++ src/main/java/ReplitsPart2/replits135.java
@@ -0,0 +1,12 @@
+package ReplitsPart2;
+
+public class replits135 {public static void main(String[] args){
+    System.out.println(surround("abcabcabc","c")); //"ab(c)ab(c)ab(c)"
+    System.out.println(surround("technology","o")); //"techn(o)l(o)gy"
+}
+
+    static String surround(String s,String c){
+        return s.replaceAll(c,"("+c+")");
+    }
+
+}
---
+++ src/main/java/ReplitsPart2/replits137.java
@@ -0,0 +1,21 @@
+package ReplitsPart2;
+
+public class replits137 {
+        private String name="John";
+        String city="Miami";
+        protected String schoolN="Syntax";
+        public int batchN=9;
+
+    void displayInfo(){
+        System.out.println("My name is "+name+" and I live in "+city+". I study at "+schoolN+" in batch "+batchN);
+    }
+
+    public static void main(String[] args) {
+
+        replits137 obj=new replits137();
+        obj.displayInfo();
+
+    }}
+
+
+
